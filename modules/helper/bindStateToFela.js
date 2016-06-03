b77a6d59207a28009bdd13dcfd7adcca80b7062d
Robin Frischmann <robin@rofrischmann.de>
2016-06-03 17:00:37 +0200

    bindState HoC

---
+++ modules/helper/bindStateToFela.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+
+const defaultMapper = state => state
+
+export default function bindStateToFela(mapper = defaultMapper) {
+  return component => {
+    // handle functional Components
+    if (!component.prototype.setState) {
+      console.warn('Does not work with functional Components.')
+    }
+
+    // handle stateful class Components
+    class EnhancedComponent extends component {
+      render() {
+        this.fela = (selector, additionalProps, plugins) => this.context.fela(selector, additionalProps ? {
+          ...mapper(this.state),
+          ...additionalProps
+        } : mapper(this.state), plugins)
+        return super.render()
+      }
+    }
+
+    EnhancedComponent.contextTypes = {
+      ...component.contextTypes,
+      fela: PropTypes.func.isRequired
+    }
+
+    return EnhancedComponent
+  }
+}
